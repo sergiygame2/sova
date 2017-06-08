bf88410e9b9177b7eb63b3b371b0034d36b996bb
Sergiy Sukharskyi <sergiy.sukharskyi@newtonideas.com>
2017-06-08 20:06:22 +0300

    Implemented UsersGyms, added wwwroot/images to gitignore (temporary)

---
+++ SportApp/Models/UsersGyms.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SportApp.Models
+{
+    public class UsersGyms
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+
+        public int GymId { get; set; }
+        public Gym Gym { get; set; }
+    }
+}
