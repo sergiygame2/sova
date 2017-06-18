6bfc2f3dde91dcab5dec8284ab8951bc1fd14f4e
OleksandraBosa <sankasufy@gmail.com>
2017-06-19 01:15:36 +0300

    some changes to search resluts page

--- SportApp/wwwroot/js/Site.js
+++ SportApp/wwwroot/js/Site.js
@@ -60,6 +60,16 @@ function initialize(gyms) {
                     infowindow.open(map, marker);
                 });
         });
+
+    $(".dropdown dt a").on('click', function () {
+        $(".dropdown dd ul").slideToggle('fast');
+    });
+
+    $(".dropdown dd ul li a").on('click', function () {
+        $(".dropdown dd ul").hide();
+    });
+
+    
 <<<<<<< HEAD
 
 
@@ -67,4 +77,31 @@ function initialize(gyms) {
 >>>>>>> 395baf752fb0b4ea7ba3eea90da599fa79cd2a36
 } 
 
+/*
+function getSelectedValue(id) {
+    return $("#" + id).find("dt a span.value").html();
+}
 
+$(document).bind('click', function (e) {
+    var $clicked = $(e.target);
+    if (!$clicked.parents().hasClass("dropdown")) $(".dropdown dd ul").hide();
+});
+
+$('.mutliSelect input[type="checkbox"]').on('click', function () {
+
+    var title = $(this).closest('.mutliSelect').find('input[type="checkbox"]').val(),
+        title = $(this).val() + ",";
+
+    if ($(this).is(':checked')) {
+        var html = '<span title="' + title + '">' + title + '</span>';
+        $('.multiSel').append(html);
+        $(".hida").hide();
+    } else {
+        $('span[title="' + title + '"]').remove();
+        var ret = $(".hida");
+        $('.dropdown dt a').append(ret);
+
+    }
+});
+
+*/
